428f160d5b807ca4f823a8637166a008b5bdd8c1
Robinhood-r <danialraeen@gmail.com>
2026-07-20 01:08:21 +1000

    added comments in the CLI file

--- cli.py
+++ cli.py
@@ -8,6 +8,8 @@ parser.add_argument("--url", help="URL to scan")
 parser.add_argument("--email", help="Email to scan")
 args = parser.parse_args()
 
+
+# Checking the given URL
 if args.url:
     print(f"\nTornet - Scanning URL: {args.url}")
     print("─" * 40)
@@ -15,6 +17,7 @@ if args.url:
     for i in results:
         print(f"{i}")
 
+# Checking the given email
 if args.email:
     print(f"\nTornet - Scanning Email: {args.email}")
     print("─" * 40)
